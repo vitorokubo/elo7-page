53461cd512877ab359273b98d48fd5109ea758ba
Vitor Sabatin <vitor.okubo@hotmail.com>
2023-01-11 04:01:36 -0300

    feat(herobanner): criação do componente herobanner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import './App.sass'
+import HeroBanner from './pages/job-page/herobanner/HeroBanner'
 
 function App() {
     const [count, setCount] = useState(0)
 
-    return <div className="App">Hello World</div>
+    return (
+        <div className="App">
+            <HeroBanner></HeroBanner>
+        </div>
+    )
 }
 
 export default App
---
+++ src/pages/job-page/herobanner/HeroBanner.jsx
@@ -0,0 +1,14 @@
+import './styles.sass'
+
+export default function HeroBanner() {
+    return (
+        <header>
+            <div
+                className="banner"
+                alt="Banner com foto dos colaboradores da Elo7 reunidos e em comemoração"
+            >
+                <h1>Trabalhe no Elo7</h1>
+            </div>
+        </header>
+    )
+}
